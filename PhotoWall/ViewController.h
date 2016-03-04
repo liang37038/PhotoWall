acd4ef61c0a43e9c24d8ed3a87b2b0f65410cab5
Richard <richard@ganguo.hk>
2016-03-04 14:02:09 +0800

    Initial Commit

---
+++ PhotoWall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoWall
+//
+//  Created by Richard Liang on 16/3/4.
+//  Copyright © 2016年 lwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
